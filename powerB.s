9de4abe1d2edef6563983b598c271aa323d102ba
Despan Alex <gargant@LAPTOP-450TNJ1O.localdomain>
2022-09-09 17:54:54 +0200

    removed some redundant code

--- powerB.s
+++ powerB.s
@@ -26,6 +26,10 @@ main:
 	popq %rbp           # restoring base pointer location
 
 end:
+
+	movq %rbp, %rsp     # copy rbp to rsp
+	popq %rbp           # popping rbp (clear stack)
+	
 	# exit code
 	movq $0, %rdi       # setting the exit code (kinda like return 0 in c++)
 	call exit
@@ -62,15 +66,10 @@ even:
 	movq $output_even, %rdi # first argument
 	call printf         # calling function 
 	
-	jmp clear_stack     # jumping to clear_stack label
+	jmp end     # jumping to end label
 odd:
 	movq $0, %rax       # no varargs for printf
 	movq $output_odd, %rdi # param 1 output string
 	call printf         # actual function calling
 	
-	jmp clear_stack     # jumping to clear_stack label
-clear_stack:
-	movq %rbp, %rsp     # copy rbp to rsp
-	popq %rbp           # popping rbp (clearing stack)
-	
-	ret                 # returning
+	jmp end     # jumping to end
